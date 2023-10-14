70469d95842f8a6bc33c71152ad9910adee9cdb9
Yonghun <yonghuni484@gmail.com>
2023-10-14 22:20:11 +0900

    Feat: Add Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "",
+      },
+    }),
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ChakraProvider>
+  // </React.StrictMode>
 );
--- src/pages/Dashboard/staff/Dashboard_S.jsx
+++ src/pages/Dashboard/staff/Dashboard_S.jsx
@@ -1,9 +1,13 @@
 import * as style from "./Dashboard_S.style";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const Dashboard_S = () => {
   return (
     <style.DashboardWrap>
       <h1>Dashboard_S</h1>
+      <Button colorScheme="teal" size="md">
+        Button
+      </Button>
     </style.DashboardWrap>
   );
 };
