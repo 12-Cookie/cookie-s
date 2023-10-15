444c802d5a572d46c85a07e83bfcb38152cc76f9
Yonghun <yonghuni484@gmail.com>
2023-10-15 21:19:37 +0900

    Feat: Add useFireFetch

--- src/pages/Dashboard/staff/Dashboard_S.jsx
+++ src/pages/Dashboard/staff/Dashboard_S.jsx
@@ -1,13 +1,39 @@
 import * as style from "./Dashboard_S.style";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useFireFetch } from "../../../hooks/useFireFetch";
 
 const Dashboard_S = () => {
+  const fetch = useFireFetch();
+  const user = fetch.getData("users", "isAdmin", false);
+
+  const handleGetClick = () => {
+    console.log(user);
+  };
+
+  const handleSetClick = () => {
+    fetch.postData("users", "1234", {
+      id: "1234",
+      isAdmin: false,
+      name: "유저test",
+    });
+  };
+
   return (
     <style.DashboardWrap>
       <h1>Dashboard_S</h1>
-      <Button colorScheme="teal" size="md">
-        Button
-      </Button>
+      <div style={{ marginBottom: "30px" }}>
+        <Button colorScheme="teal" size="md" onClick={handleGetClick}>
+          get
+        </Button>
+      </div>
+      <div>
+        <Button colorScheme="teal" size="md" onClick={handleSetClick}>
+          set
+        </Button>
+      </div>
+      {user.map((v, i) => {
+        return <div key={i}>{v.name}</div>;
+      })}
     </style.DashboardWrap>
   );
 };
