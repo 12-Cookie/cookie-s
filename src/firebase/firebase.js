151f0187dea416547baec43fcca43182e99b674e
Yonghun <yonghuni484@gmail.com>
2023-10-15 14:12:30 +0900

    Feat: Add firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: "cookie-15f01.firebaseapp.com",
+  projectId: "cookie-15f01",
+  storageBucket: "cookie-15f01.appspot.com",
+  messagingSenderId: "177630679531",
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: "G-B4MT8RZG4H",
+};
+
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
