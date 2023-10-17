eb202b0be7599e3fcd552546ccff79f979617680
Yonghun <yonghuni484@gmail.com>
2023-10-17 21:36:52 +0900

    Feat: Add schedule date data in AssignRoleHeader

--- src/components/Schedule/admin/assignRole/AssignHeader.jsx
+++ src/components/Schedule/admin/assignRole/AssignHeader.jsx
@@ -1,12 +1,27 @@
 import * as style from "./AssignHeader.style";
+import getDayOfWeek from "../../../../utils/getDayOfWeek";
+import amOrPm from "../../../../utils/amOrPm";
 
-const AssignHeader = () => {
+const AssignHeader = ({ schedule }) => {
+  const dayText = getDayOfWeek(
+    schedule.date.year,
+    schedule.date.month,
+    schedule.date.day,
+  );
+
   return (
     <style.AssignHeaderWrap>
-      <style.AssignHeaderTop>2023.10.10(수)</style.AssignHeaderTop>
+      <style.AssignHeaderTop>
+        {schedule.date.year}.{schedule.date.month}.{schedule.date.day}({dayText}
+        )
+      </style.AssignHeaderTop>
       <style.AssignHeaderBottom>
-        <span>오전 10:00</span>
-        <span>오후 18:00</span>
+        <span>
+          {amOrPm(schedule.time.start)} {schedule.time.start}
+        </span>
+        <span>
+          {amOrPm(schedule.time.end)} {schedule.time.end}
+        </span>
       </style.AssignHeaderBottom>
     </style.AssignHeaderWrap>
   );
--- src/pages/Schedule/admin/assignRole/AssignRole.jsx
+++ src/pages/Schedule/admin/assignRole/AssignRole.jsx
@@ -1,11 +1,20 @@
 import * as style from "./AssignRole.style";
 import AssignHeader from "../../../../components/Schedule/admin/assignRole/AssignHeader";
+import { useFireFetch } from "../../../../hooks/useFireFetch";
 
 const AssignRole = () => {
+  const fireFetch = useFireFetch();
+
+  const schedule = fireFetch.getData(
+    "schedule",
+    "id",
+    "NyOKqZmAHtErt68DHrTO",
+  )[0];
+
   return (
     <style.AssignRoleWrap>
       <h1 style={{ marginBottom: "1rem" }}>역할배정</h1>
-      <AssignHeader />
+      {schedule && <AssignHeader schedule={schedule} />}
     </style.AssignRoleWrap>
   );
 };
