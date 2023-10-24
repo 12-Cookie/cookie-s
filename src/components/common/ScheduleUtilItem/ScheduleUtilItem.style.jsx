626d1720999b056ee27ecf8ed1364990a1039c8f
Taegwan Youn <tkyoun0421@naver.com>
2023-10-24 10:20:04 +0900

    Design: ScheduleUtipWrap button -> div change

--- src/components/common/ScheduleUtilItem/ScheduleUtilItem.style.jsx
+++ src/components/common/ScheduleUtilItem/ScheduleUtilItem.style.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ScheduleUtilWrap = styled.button`
+export const ScheduleUtilWrap = styled.div`
   margin-left: auto;
 `;
 export const ScheduleUtilBtn = styled.button`
