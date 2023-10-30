1363f65b920890b7955fb61a5503c112ad447004
joanShim <joanjeongahshim@gmail.com>
2023-10-31 01:03:41 +0900

    Feat: 정보 기능 정의

--- src/pages/Info/admin/Info_A.jsx
+++ src/pages/Info/admin/Info_A.jsx
@@ -1,13 +1,21 @@
 import * as style from "./Info_A.style";
 import { app } from "../../../firebase/firebase";
 import { signOut, getAuth } from "firebase/auth";
-import { Button, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Spacer, Text, Tag } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "../../../store/user/useUserStore";
+import { useFireFetch } from "../../../hooks/useFireFetch";
 
 const Info_A = () => {
   const auth = getAuth(app);
   const navigate = useNavigate();
+  const userStore = useUserStore();
+
+  const fetch = useFireFetch();
+  const currentCompanyId = userStore.userData.companyId;
+  const companyInfo = fetch.getData("company").find((company) => {
+    return company.id === currentCompanyId;
+  });
 
   const handleLogOut = () => {
     signOut(auth)
@@ -22,13 +30,28 @@ const Info_A = () => {
   };
   return (
     <style.InfoWrap>
-      <h1>회사정보</h1>
-      <Flex>
+      <Heading as="h2" size="md" mb="2rem">
+        공지사항
+      </Heading>
+      <Flex mb={4}>
         <Heading as="h5" size="sm">
           회사명
         </Heading>
         <Spacer />
-        <Text></Text>
+        <Text>{companyInfo.name}</Text>
+      </Flex>
+      <Flex mb={4}>
+        <Heading as="h5" size="sm">
+          포지션
+        </Heading>
+        <Spacer />
+        <Flex flexWrap="wrap">
+          {companyInfo.roles.map((role, index) => (
+            <Tag key={index} size="sm" marginLeft="2" marginBottom="2">
+              {role}
+            </Tag>
+          ))}
+        </Flex>
       </Flex>
       <Button onClick={handleLogOut}>로그아웃</Button>
     </style.InfoWrap>
