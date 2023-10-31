5b0361793a2da4b20b7380d28b99c7d4e140310c
chaeminseok <minseok23491@naver.com>
2023-10-31 13:55:48 +0900

    Update

--- src/pages/Schedule/staff/schedule/Schedule_S.jsx
+++ src/pages/Schedule/staff/schedule/Schedule_S.jsx
@@ -1,9 +1,34 @@
+import { Button, Heading } from "@chakra-ui/react";
 import * as style from "./Schedule_S.style";
+import { Link } from "react-router-dom";
+import StaffCalendar from "../../../../components/Schedule/staff/staffCalendar";
+import { useState } from "react";
 
 const Schedule_S = () => {
+  const [value, onChange] = useState(new Date());
   return (
     <style.ScheduleWrap>
-      <h1>Schedule_S</h1>
+      <Heading as="h2" size="md" mb="1rem">
+        스케줄 관리
+      </Heading>
+      <div style={{ marginBottom: "1rem" }}>
+        <Button borderRadius="2rem " colorScheme="blue" color="white" size="md">
+          스케줄 생성
+        </Button>
+        <Link to="/schedule/manage">
+          <Button
+            borderRadius="2rem "
+            colorScheme="white"
+            color="black"
+            size="md"
+          >
+            신청 확인
+          </Button>
+        </Link>
+      </div>
+      <div>
+        <StaffCalendar value={value} onChange={onChange} />
+      </div>
     </style.ScheduleWrap>
   );
 };
