d700ddce29d508baa27760957d0ccb79b6ae5242
joanShim <joanjeongahshim@gmail.com>
2023-11-02 16:44:36 +0900

    Feat: 유저 정보 페이지 구현

--- src/pages/Info/staff/Info_S.jsx
+++ src/pages/Info/staff/Info_S.jsx
@@ -1,13 +1,24 @@
 import * as style from "./Info_S.style";
 import { app } from "../../../firebase/firebase";
 import { signOut, getAuth } from "firebase/auth";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Spacer,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "../../../store/user/useUserStore";
 
 const Info_S = () => {
   const auth = getAuth(app);
   const navigate = useNavigate();
+  const userStore = useUserStore();
+
+  const myInfo = userStore.userData;
 
   const handleLogOut = () => {
     signOut(auth)
@@ -22,8 +33,46 @@ const Info_S = () => {
   };
   return (
     <style.InfoWrap>
-      <h1>info_s</h1>
-      <Button onClick={handleLogOut}>로그아웃</Button>
+      <Heading as="h2" size="md" mb="2rem">
+        내 정보
+      </Heading>
+      {myInfo ? (
+        <>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              회사명
+            </Heading>
+            <Spacer />
+            <Text>{myInfo.name}</Text>
+          </Flex>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              주소
+            </Heading>
+            <Spacer />
+            <Text>{myInfo.address}</Text>
+          </Flex>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              생년월일
+            </Heading>
+            <Spacer />
+            <Text>{myInfo.birthDate}</Text>
+          </Flex>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              시급
+            </Heading>
+            <Spacer />
+            <Text>{myInfo.payPerHour}</Text>
+          </Flex>
+          <Center>
+            <Button onClick={handleLogOut}>로그아웃</Button>
+          </Center>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </style.InfoWrap>
   );
 };
