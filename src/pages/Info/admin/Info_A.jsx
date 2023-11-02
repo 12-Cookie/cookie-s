c6b78db21aedec933e8c55e7bb53923864ba84bc
joanShim <joanjeongahshim@gmail.com>
2023-11-02 16:19:09 +0900

    Feat: 직원관리페이지 구현

--- src/pages/Info/admin/Info_A.jsx
+++ src/pages/Info/admin/Info_A.jsx
@@ -33,27 +33,33 @@ const Info_A = () => {
       <Heading as="h2" size="md" mb="2rem">
         공지사항
       </Heading>
-      <Flex mb={4}>
-        <Heading as="h5" size="sm">
-          회사명
-        </Heading>
-        <Spacer />
-        <Text>{companyInfo.name}</Text>
-      </Flex>
-      <Flex mb={4}>
-        <Heading as="h5" size="sm">
-          포지션
-        </Heading>
-        <Spacer />
-        <Flex flexWrap="wrap">
-          {companyInfo.roles.map((role, index) => (
-            <Tag key={index} size="sm" marginLeft="2" marginBottom="2">
-              {role}
-            </Tag>
-          ))}
-        </Flex>
-      </Flex>
-      <Button onClick={handleLogOut}>로그아웃</Button>
+      {companyInfo ? (
+        <>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              회사명
+            </Heading>
+            <Spacer />
+            <Text>{companyInfo.name}</Text>
+          </Flex>
+          <Flex mb={4}>
+            <Heading as="h5" size="sm">
+              포지션
+            </Heading>
+            <Spacer />
+            <Flex flexWrap="wrap">
+              {companyInfo.roles.map((role, index) => (
+                <Tag key={index} size="sm" marginLeft="2" marginBottom="2">
+                  {role}
+                </Tag>
+              ))}
+            </Flex>
+          </Flex>
+          <Button onClick={handleLogOut}>로그아웃</Button>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </style.InfoWrap>
   );
 };
